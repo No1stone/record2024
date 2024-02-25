db6537a2a118fb0fe0d30ac016d81a13caa43960
jangwonseok <jangws1003@naver.com>
2024-02-25 17:31:02 +0900

    feat(qt) : 조회 추가.

--- src/main/java/com/origemite/querytouble/biz/controller/case1/Case1Controller.java
+++ src/main/java/com/origemite/querytouble/biz/controller/case1/Case1Controller.java
@@ -1,10 +1,14 @@
 package com.origemite.querytouble.biz.controller.case1;
 
+import com.origemite.querytouble.biz.controller.case1.vo.ReqCase1;
 import com.origemite.querytouble.biz.service.case1.Case1Service;
-import com.origemite.querytouble.data.jdbc.JdbcTemplateRepo;
+import com.origemite.querytouble.data.db.entity.case1.Case1View;
+import com.origemite.querytouble.data.jdbc.case1.Case1JdbcRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -13,20 +17,28 @@ import org.springframework.web.bind.annotation.*;
 public class Case1Controller {
 
     private final Case1Service case1Service;
-    private final JdbcTemplateRepo jdbcTemplateRepo;
+    private final Case1JdbcRepo case1JdbcRepo;
 
     @PostMapping("")
-    public void Case1TableCreate(){
-        jdbcTemplateRepo.case1Table();
-        jdbcTemplateRepo.case1ViewTable();
+    public void Case1TableCreate() {
+        case1JdbcRepo.case1Table();
+        case1JdbcRepo.case1ViewTable();
     }
+
     @DeleteMapping("")
-    public void Case1TableDelete(){
-        jdbcTemplateRepo.dropCase1();
+    public void Case1TableDelete() {
+        case1JdbcRepo.dropCase1();
     }
 
     @GetMapping("/create-data")
-    public void CreateData(){
+    public void CreateData() {
         case1Service.CreateData();
     }
+
+    @GetMapping("")
+    public List<Case1View> select(@ModelAttribute ReqCase1 dto) {
+        return case1Service.select(dto);
+    }
+
+
 }
