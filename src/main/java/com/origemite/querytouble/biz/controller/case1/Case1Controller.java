c3be71121f91ea9312b234ef3a91942bc7e7c42d
jangwonseok <jangws1003@naver.com>
2024-02-25 16:09:52 +0900

    feat(qt) : case1 controller edit..

--- src/main/java/com/origemite/querytouble/biz/controller/case1/Case1Controller.java
+++ src/main/java/com/origemite/querytouble/biz/controller/case1/Case1Controller.java
@@ -4,21 +4,29 @@ import com.origemite.querytouble.biz.service.case1.Case1Service;
 import com.origemite.querytouble.data.jdbc.JdbcTemplateRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/case1")
 public class Case1Controller {
 
     private final Case1Service case1Service;
     private final JdbcTemplateRepo jdbcTemplateRepo;
 
-    @GetMapping("/test1")
-    public void TestTableCreate(){
+    @PostMapping("")
+    public void Case1TableCreate(){
         jdbcTemplateRepo.case1Table();
         jdbcTemplateRepo.case1ViewTable();
     }
+    @DeleteMapping("")
+    public void Case1TableDelete(){
+        jdbcTemplateRepo.dropCase1();
+    }
 
+    @GetMapping("/create-data")
+    public void CreateData(){
+        case1Service.CreateData();
+    }
 }
