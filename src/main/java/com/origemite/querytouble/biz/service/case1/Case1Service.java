9b3e26586061db8a0a391a985bf576b15274fa80
jangwonseok <jangws1003@naver.com>
2024-02-25 17:31:20 +0900

    feat(qt) : 조회 서비스 추가

--- src/main/java/com/origemite/querytouble/biz/service/case1/Case1Service.java
+++ src/main/java/com/origemite/querytouble/biz/service/case1/Case1Service.java
@@ -1,14 +1,17 @@
 package com.origemite.querytouble.biz.service.case1;
 
+import com.origemite.querytouble.biz.controller.case1.vo.ReqCase1;
 import com.origemite.querytouble.data.db.entity.case1.Case1Customer;
 import com.origemite.querytouble.data.db.entity.case1.Case1Group;
 import com.origemite.querytouble.data.db.entity.case1.Case1User;
+import com.origemite.querytouble.data.db.entity.case1.Case1View;
 import com.origemite.querytouble.data.db.repo.case1.Case1CustomerRepo;
 import com.origemite.querytouble.data.db.repo.case1.Case1GroupRepo;
 import com.origemite.querytouble.data.db.repo.case1.Case1UserRepo;
 import com.origemite.querytouble.data.db.repo.case1.Case1ViewRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,15 +36,19 @@ public class Case1Service {
         List<Case1Customer> cust = new ArrayList<>();
         List<Case1Group> grp = new ArrayList<>();
         List<Case1User> usr = new ArrayList<>();
-        for(int i = 0 ; i < 100 ; i++){
+        for (int i = 0; i < 100; i++) {
             LocalDateTime ldt = LocalDateTime.now();
-            cust.add(new Case1Customer(UUID.randomUUID().toString(),"CUST"+i,ldt,ldt));
-            grp.add(new Case1Group(UUID.randomUUID().toString(),"GRP"+i,ldt,ldt));
-            usr.add(new Case1User(UUID.randomUUID().toString(),"USR"+i,ldt,ldt));
+            cust.add(new Case1Customer(UUID.randomUUID().toString(), "CUST" + i, ldt, ldt));
+            grp.add(new Case1Group(UUID.randomUUID().toString(), "GRP" + i, ldt, ldt));
+            usr.add(new Case1User(UUID.randomUUID().toString(), "USR" + i, ldt, ldt));
         }
         case1CustomerRepo.saveAll(cust);
         case1GroupRepo.saveAll(grp);
         case1UserRepo.saveAll(usr);
 
+    }
+
+    public List<Case1View> select(ReqCase1 dto) {
+        return case1ViewRepo.findBy(PageRequest.of(dto.getPage(), dto.getSize())).stream().toList();
     }
 }
