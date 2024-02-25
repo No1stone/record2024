e6655d783ec265bb850291a867f98aaf1868dab2
jangwonseok <jangws1003@naver.com>
2024-02-25 15:30:39 +0900

    feat(qt) : 생성쿼리

--- src/main/java/com/origemite/querytouble/biz/controller/case1/Case1Controller.java
+++ src/main/java/com/origemite/querytouble/biz/controller/case1/Case1Controller.java
@@ -1,8 +1,10 @@
 package com.origemite.querytouble.biz.controller.case1;
 
 import com.origemite.querytouble.biz.service.case1.Case1Service;
+import com.origemite.querytouble.data.jdbc.JdbcTemplateRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class Case1Controller {
 
     private final Case1Service case1Service;
+    private final JdbcTemplateRepo jdbcTemplateRepo;
 
+    @GetMapping("/test1")
+    public void TestTableCreate(){
+        jdbcTemplateRepo.case1Table();
+        jdbcTemplateRepo.case1ViewTable();
+    }
 
 }
