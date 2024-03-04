a6d8da9fdb0a7e732e274d6d1f00ee4f30892479
jangwonseok <jangws1003@naver.com>
2024-03-04 14:34:50 +0900

    feat(qt) : Dsl interface

---
+++ src/main/java/com/origemite/querytouble/data/db/repo/case2/dsl/Case2EndRepoDsl.java
@@ -0,0 +1,11 @@
+package com.origemite.querytouble.data.db.repo.case2.dsl;
+
+import com.origemite.querytouble.biz.controller.case2.vo.Case2SelectAll;
+
+import java.util.List;
+
+public interface Case2EndRepoDsl {
+
+    List<Case2SelectAll> selectALL();
+
+}
