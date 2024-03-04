dcef18bb9220ee04b640bd14fbc1e16fb7727bbd
jangwonseok <jangws1003@naver.com>
2024-03-04 14:34:41 +0900

    feat(qt) : Dsl add

--- src/main/java/com/origemite/querytouble/data/db/repo/case2/Case2EndRepo.java
+++ src/main/java/com/origemite/querytouble/data/db/repo/case2/Case2EndRepo.java
@@ -1,7 +1,8 @@
 package com.origemite.querytouble.data.db.repo.case2;
 
 import com.origemite.querytouble.data.db.entity.case2.Case2End;
+import com.origemite.querytouble.data.db.repo.case2.dsl.Case2EndRepoDsl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Case2EndRepo extends JpaRepository<Case2End, String> {
+public interface Case2EndRepo extends JpaRepository<Case2End, String> , Case2EndRepoDsl {
 }
