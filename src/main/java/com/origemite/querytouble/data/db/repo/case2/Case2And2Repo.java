fbccd5a6618861853a706d8597ef63f12fb21a0f
jangwonseok <jangws1003@naver.com>
2024-03-04 13:12:44 +0900

    feat(qt) : repo 추가

---
+++ src/main/java/com/origemite/querytouble/data/db/repo/case2/Case2And2Repo.java
@@ -0,0 +1,8 @@
+package com.origemite.querytouble.data.db.repo.case2;
+
+import com.origemite.querytouble.data.db.entity.case2.Case2And1;
+import com.origemite.querytouble.data.db.entity.case2.Case2And2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Case2And2Repo extends JpaRepository<Case2And2, String> {
+}
