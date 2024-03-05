ece98e7de560d2f500dae7d195e08e456816c855
jangwonseok <jangws1003@naver.com>
2024-03-05 14:40:21 +0900

    컨트롤러

---
+++ src/main/java/com/origemite/jooqexame/biz/JooqController.java
@@ -0,0 +1,15 @@
+package com.origemite.jooqexame.biz;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class JooqController {
+
+//    private final DSLContext dslContext;
+
+}
