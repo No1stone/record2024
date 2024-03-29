a0fd5204efa5b218df87c114c21ea2d35e64c351
jangwonseok <jangws1003@naver.com>
2024-03-29 17:15:11 +0900

    readme add

--- src/main/java/com/origemite/zipkinexame/ZipkinController.java
+++ src/main/java/com/origemite/zipkinexame/ZipkinController.java
@@ -2,6 +2,7 @@ package com.origemite.zipkinexame;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class ZipkinController {
 
     @GetMapping("/test1")
-    public String test1(){
+    public String test1() {
         log.info("test1 controller");
         return "test1";
+    }
+
+    @GetMapping("/test2")
+    public String test2() throws BadRequestException {
+        log.info("test1 controller");
+        if (true) {
+            try {
+                throw new BadRequestException();
+            }
+            catch (Exception e){
+                log.error("error test");
+            }
+            throw new BadRequestException();
+        }
+        return "test2";
     }
 
 }
