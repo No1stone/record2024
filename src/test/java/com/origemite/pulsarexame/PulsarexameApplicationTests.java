7a01258e6381088d3ae484c63f63faf9edf94ace
jangwonseok <jangws1003@naver.com>
2024-04-06 14:34:46 +0900

    first coommit

---
+++ src/test/java/com/origemite/pulsarexame/PulsarexameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origemite.pulsarexame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarexameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
