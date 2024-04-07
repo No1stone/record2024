70d79caae2568fc8ca1d6e9d4730398985b23890
jangwonseok <jangws1003@naver.com>
2024-04-07 17:57:39 +0900

    subtree 정리

---
+++ portfolio2024/zipkinexame/src/test/java/com/origemite/zipkinexame/ZipkinexameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origemite.zipkinexame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinexameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
