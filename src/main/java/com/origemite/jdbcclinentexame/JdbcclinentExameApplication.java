bfdc41e13e4714ffae409b444113713b52462553
jangwonseok <jangws1003@naver.com>
2024-05-01 13:25:13 +0900

    first commit

---
+++ src/main/java/com/origemite/jdbcclinentexame/JdbcclinentExameApplication.java
@@ -0,0 +1,13 @@
+package com.origemite.jdbcclinentexame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcclinentExameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcclinentExameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/origemite/jdbcclinentexame/JdbcclinentExameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origemite.jdbcclinentexame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcclinentExameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
