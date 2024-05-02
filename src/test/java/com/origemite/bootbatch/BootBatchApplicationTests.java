8a3de04389237a8d96949261ca7906552714ced9
jangwonseok <jangws1003@naver.com>
2024-05-02 21:28:43 +0900

    hello world job

--- src/test/java/com/origemite/bootbatch/BootBatchApplicationTests.java
+++ src/test/java/com/origemite/bootbatch/BootBatchApplicationTests.java
@@ -1,9 +1,11 @@
 package com.origemite.bootbatch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableBatchProcessing
 class BootBatchApplicationTests {
 
 	@Test
